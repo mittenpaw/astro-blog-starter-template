0f6badff833c5a171fc6e77e7e764ca3b7a65305
Yoshio Kawamoto <yoshio_kawamoto@hotmail.com>
2026-05-30 22:17:35 +0900

    著者名を「川本 義雄」から「リキンダ」に変更・再デプロイ

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "川本 義雄 | 個人ブログ";
+export const SITE_TITLE = "リキンダ | 個人ブログ";
 export const SITE_DESCRIPTION = "技術・日常・思考を綴るパーソナルブログ。";
-export const AUTHOR_NAME = "川本 義雄";
+export const AUTHOR_NAME = "リキンダ";
 export const AUTHOR_EMAIL = "yoshio_kawamoto@hotmail.com";
